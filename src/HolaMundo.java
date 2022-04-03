e3d6434e8a2a05513a5bb37059d3c2900fe08b7e
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2022-04-03 17:36:24 -0400

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("Mensaje de Gustavo");
+        System.out.println("Ignacio Morales");
         
     }
 }
